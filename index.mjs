5a9642a61e4decd8344cfa3b16b4bc99c219914f
David Sanchez <davidsanrubio@gmail.com>
2023-10-05 15:10:38 +0200

    [chore] Remove wrong file

--- index.mjs
+++
@@ -1,4 +0,0 @@
-import { HowLongToBeatService, HowLongToBeatEntry } from 'howlongtobeat';
-
-let hltbService = new HowLongToBeatService();
-hltbService.search('Sea of stars').then((result) => console.log(result));
