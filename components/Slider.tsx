9360338ea7d20cb3e0f8c7b3966cf19d6be1d058
David Sanchez <davidsanrubio@gmail.com>
2023-10-11 22:06:36 +0200

    [chore] Add arrows to slider

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -2,6 +2,8 @@ import { IScreenshot } from "@/lib/models/media"
 import { cn } from "@/lib/utils"
 import Image from "next/image"
 import { useState } from "react"
+import Chevron from "./icons/Chevron"
+import Link from "next/link"
 
 interface Props {
     screenshots: IScreenshot[]
@@ -9,6 +11,20 @@ interface Props {
 
 const Slider = ({ screenshots }: Props) => {
     const [slide, setSlide] = useState<string>(screenshots[0]?.image_id)
+
+    const previousUrl = () => {
+        const index = screenshots.findIndex(s => s.image_id === slide)
+        return screenshots.at(index - 1)?.image_id
+    }
+    
+    const nextUrl = () => {
+        const index = screenshots.findIndex(s => s.image_id === slide)
+        return index === screenshots.length - 1 ?
+            screenshots[0].image_id : screenshots.at(index + 1)?.image_id
+    }
+
+    const previous = () => setSlide(previousUrl() ?? screenshots[screenshots.length - 1].image_id)
+    const next = () => setSlide(nextUrl() ?? screenshots[0].image_id)
 
     return <div className="flex flex-col items-center aspect-video">
         <div className="w-full bg-white rounded overflow-x-hidden flex snap-x h-full">
@@ -22,6 +38,15 @@ const Slider = ({ screenshots }: Props) => {
                     quality={100}
                 />
             </div> )}
+        </div>
+
+        <div className="absolute w-full h-full flex justify-between items-center p-4">
+            <Link href={`#${previousUrl()}`} onClick={previous} className="p-0.5 bg-white text-black rounded-full cursor-pointer opacity-70 hover:opacity-100 transition-all">
+                <Chevron direction="left" className="pr-0.5 w-7 h-7"/>
+            </Link>
+            <Link href={`#${nextUrl()}`} onClick={next} className="p-0.5 bg-white text-black rounded-full cursor-pointer opacity-70 hover:opacity-100 transition-all">
+                <Chevron direction="right" className="pl-0.5 w-7 h-7"/>
+            </Link>
         </div>
     
         <div className="flex absolute bottom-5 gap-3 bg-white rounded-full py-2 px-3 opacity-70 hover:opacity-100 transition-all">
---
+++ components/icons/Chevron.tsx
@@ -0,0 +1,24 @@
+interface Props {
+    direction: 'up' | 'down' | 'left' | 'right'
+}
+
+const Chevron = ({ direction = 'up', ...props }) => <svg width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" {...props}>
+    { direction === 'up' ? <>
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M6 15l6 -6l6 6"></path>
+    </> : <></> }
+    { direction === 'right' ? <>
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M9 6l6 6l-6 6"></path>
+    </> : <></> }
+    { direction === 'down' ? <>
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M6 9l6 6l6 -6"></path>
+    </> : <></> }
+    { direction === 'left' ? <>
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M15 6l-6 6l6 6"></path>
+    </> : <></> }
+</svg>
+
+export default Chevron
