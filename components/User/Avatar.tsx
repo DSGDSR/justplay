4d6a0ef654c40009ce51c30f2cb00636d53477e6
David Sanchez <davidsanrubio@gmail.com>
2023-10-22 13:45:14 +0200

    [feature] Add 'view profile' menu item in users dropdown

--- components/User/Avatar.tsx
+++ components/User/Avatar.tsx
@@ -1,7 +1,13 @@
 "use client"
 
+import { Children, Fragment, cloneElement, createElement, useEffect } from "react";
 import { Button } from "../Button"
 import { UserButton, SignedIn, SignedOut, ClerkLoaded, useClerk } from "@clerk/nextjs";
+import { cn, replaceNodeWithReactComponent } from "@/lib/utils";
+import { render } from "react-dom";
+import { createPortal } from "react-dom";
+import { createRoot } from "react-dom/client";
+import Link from "next/link";
 
 const avatarSize = {
     width: '2.15rem',
@@ -17,7 +23,33 @@ const elements = {
 const UserAvatar = () => {
     const clerk = useClerk()
 
-    return <div className='relative min-w-[2.25rem] h-9'>
+    const addProfileButton = () => {
+        const userDropdown = document.querySelector('.cl-userButtonPopoverActions')
+        if (userDropdown) {
+            const root = createRoot(document.createElement('div'));
+            root.render(createPortal(<ViewProfile
+                classes={{
+                    parent: Array.from(userDropdown.children[0].classList).join(' '),
+                    icon: Array.from(userDropdown.children[0].children[0].classList).join(' '),
+                    svg: Array.from(userDropdown.children[0].children[0].children[0].classList).join(' '),
+                    text: Array.from(userDropdown.children[0].children[1].classList).join(' '),
+                }} />, userDropdown));
+
+            userDropdown.children[0]?.classList.add('order-2')
+            userDropdown.children[1]?.classList.add('order-3')
+        }
+    }
+
+    useEffect(() => {
+        document.querySelector('#user-avatar')?.addEventListener('click', addProfileButton)
+
+        return () => {
+            document.querySelector('#user-avatar')?.removeEventListener('click', addProfileButton)
+        }
+    }, [])
+
+
+    return <div className='relative min-w-[2.25rem] h-9' id="user-avatar">
         <div className='absolute top-0 left-0 w-9 h-9 rounded-full bg-muted z-[0] flex justify-center items-end'></div>
         <ClerkLoaded>
             <SignedIn>
@@ -29,5 +61,17 @@ const UserAvatar = () => {
         </ClerkLoaded>
     </div>
 }
+
+const ViewProfile = ({ classes }: any) => <Link href={`/profile`} className={classes.parent} role="menuitem">
+    <div className={classes.icon}>
+        <svg className={cn(classes.svg, "w-4 h-4")} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
+            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+        </svg>
+    </div>
+
+    <span className={classes.text}>View profile</span>    
+</Link>
 
 export default UserAvatar
