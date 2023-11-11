33a7d384b13b14d734275174da7388f20ae149fb
David Sanchez <davidsanrubio@gmail.com>
2023-11-11 15:07:23 +0100

    [feature] Add accesibility for mobile tooltips

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,12 +1,28 @@
-"use client"
+'use client'
 
-import * as TooltipPrimitive from "@radix-ui/react-tooltip"
-import { cn } from "@/lib/utils"
-import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react"
+import * as TooltipPrimitive from '@radix-ui/react-tooltip'
+import { cn } from '@/lib/utils'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef, useState } from 'react'
 
 const TooltipProvider = TooltipPrimitive.Provider
 
-const Tooltip = TooltipPrimitive.Root
+interface TooltipProps extends TooltipPrimitive.TooltipProps {
+  className?: string
+}
+
+const Tooltip = (props: TooltipProps) => {
+  const [open, setOpen] = useState(false)
+
+  return (
+      <TooltipPrimitive.Root
+        open={open}
+        onOpenChange={setOpen}
+        {...props}
+      >
+          <div onClick={() => setOpen(true)} className={props.className}>{props.children}</div>
+      </TooltipPrimitive.Root>
+  )
+}
 
 const TooltipTrigger = TooltipPrimitive.Trigger
 
@@ -18,7 +34,7 @@ const TooltipContent = forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      "z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      'z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
       className
     )}
     {...props}
