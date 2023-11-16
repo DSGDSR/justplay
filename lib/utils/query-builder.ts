9a4e21d20e5b030fcac4b56af18ac48611df7451
David Sanchez <davidsanrubio@gmail.com>
2023-11-16 08:38:16 +0100

    [chore] Create query builder class for apicalypse

---
+++ lib/utils/query-builder.ts
@@ -0,0 +1,93 @@
+class Query {
+    private query: string | undefined;
+    private queryFields: {
+        where: string[];
+        fields?: string;
+        exclude?: string;
+        sort?: string;
+        limit?: string;
+        offset?: string;
+        search?: string;
+    };
+
+    constructor() {
+        this.queryFields = {
+            where: [],
+        };
+    }
+
+    fields(fields: string | string[]): this {
+        if (fields) {
+            let fieldsString =
+                fields && Array.isArray(fields) ? fields.join(",") : fields;
+            fieldsString = fieldsString ? fieldsString.replace(/\s/g, "") : "";
+            this.queryFields.fields = `fields ${fieldsString}`;
+        }
+        return this;
+    }
+
+    exclude(exclude: string | string[]): this {
+        if (exclude) {
+            let excludeString =
+                exclude && Array.isArray(exclude) ? exclude.join(",") : exclude;
+            excludeString = excludeString ? excludeString.replace(/\s/g, "") : "";
+            this.queryFields.exclude = `exclude ${excludeString}`;
+        }
+        return this;
+    }
+
+    sort(field: string, direction?: string): this {
+        if (field) {
+            if (
+                field.toLowerCase().endsWith(" desc") ||
+                field.toLowerCase().endsWith(" asc")
+            ) {
+                this.queryFields.sort = `sort ${field}`;
+            } else {
+                this.queryFields.sort = `sort ${field} ${direction || "asc"}`;
+            }
+        }
+        return this;
+    }
+
+    limit(limit: string): this {
+        if (limit) {
+            this.queryFields.limit = `limit ${limit}`;
+        }
+        return this;
+    }
+
+    offset(offset: string): this {
+        if (offset) {
+            this.queryFields.offset = `offset ${offset}`;
+        }
+        return this;
+    }
+
+    search(search: string): this {
+        if (search) {
+            this.queryFields.search = `search "${search}"`;
+        }
+        return this;
+    }
+
+    where(filters: string | string[]): this {
+        if (filters) {
+            if (Array.isArray(filters)) {
+                this.queryFields.where.push(`where ${filters.join(" & ")}`);
+            } else {
+                this.queryFields.where.push(`where ${filters.trim()}`);
+            }
+        }
+        return this;
+    }
+
+    build(): string {
+        const { where, ...rest } = this.queryFields;
+        const query = Object.keys(this.queryFields).length > 1 || this.queryFields.where.length > 0
+            ? Object.values(rest).concat(where).join(";") + ";" : "";
+        return query;
+    }
+}
+
+export default Query;
