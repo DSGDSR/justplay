611fc86b199466a8aa7a9a38bf9ba4ae9f6efbed
David Sanchez <davidsanrubio@gmail.com>
2023-12-06 11:43:24 +0100

    [feature] Add custom list name validator

---
+++ lib/utils/validators.ts
@@ -0,0 +1,12 @@
+import {
+    string,
+    minLength,
+    maxLength,
+    custom
+} from 'valibot'
+
+export const ListNameSchemma = string([
+    minLength(3, 'List name must be at least 3 characters long'),
+    maxLength(30, 'List name must be at most 30 characters long'),
+    // TODO avoid special chars
+])
