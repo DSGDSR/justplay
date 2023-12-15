03b85dbf9ef25a4e06e2116e6d9f5afcf89d9765
David Sanchez <davidsanrubio@gmail.com>
2023-12-16 00:13:32 +0100

    [feature] Add auto scroll to cards slider

--- components/CardsSlider.tsx
+++ components/CardsSlider.tsx
@@ -2,7 +2,7 @@
 
 import { IGame } from '@/lib/models/game';
 import GameCard from './GameCard';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { cn } from '@/lib/utils';
 import Chevron from './icons/Chevron';
 import { ListsItemsResponse } from '@/lib/models/lists';
@@ -15,51 +15,70 @@ interface Props {
 }
 
 const CardsSlider = ({ games, className, lazy, lists }: Props) => {
+    const slider = useRef<HTMLDivElement>(null)
+    const [scrollInterval, setScrollInterval] = useState<NodeJS.Timeout | null>(null)
     const [scrollPosition, setScrollPosition] = useState<'left' | 'right' | 'both'>('left')
 
-    const slider = () => document.querySelector('#cards-slider')
-
     const getScrollPosition = () => {
-        return slider()?.scrollLeft ?? 0
+        return slider?.current?.scrollLeft ?? 0
     }
 
     const scrollRight = () => {
-        slider()?.scroll({
+        stopAutoScroll()
+        slider?.current?.scroll({
             left: getScrollPosition() + 450,
             behavior: 'smooth'
         })
     }
     
     const scrollLeft = () => {
-        slider()?.scroll({
+        stopAutoScroll()
+        slider?.current?.scroll({
             left: getScrollPosition() - 450,
             behavior: 'smooth'
         })
     }
 
+    const autoScroll = (direction: 'left' | 'right') => {
+        setScrollInterval(setInterval(() => {
+            if (!slider?.current) return
+
+            slider.current.scrollLeft = direction === 'left'
+                ? slider.current.scrollLeft - 2.5
+                : slider.current.scrollLeft + 2.5
+        }, 10))
+    }
+
+    const stopAutoScroll = () => {
+        if (scrollInterval) {
+            clearInterval(scrollInterval)
+            setScrollInterval(null)
+        }
+    }
+
     const listenToScroll = () => {
         if (getScrollPosition() === 0) {
             setScrollPosition('left')
-        } else if (getScrollPosition() === (slider()?.scrollWidth ?? 0) - (slider()?.clientWidth ?? 0)) {
+        } else if (getScrollPosition() === (slider?.current?.scrollWidth ?? 0) - (slider?.current?.clientWidth ?? 0)) {
             setScrollPosition('right')
         } else if (
             scrollPosition !== 'both' && (getScrollPosition() > 0 ||
-            getScrollPosition() < (slider()?.scrollWidth ?? 0) - (slider()?.clientWidth ?? 0))
+            getScrollPosition() < (slider?.current?.scrollWidth ?? 0) - (slider?.current?.clientWidth ?? 0))
         ) {
             setScrollPosition('both')
         }
     }
     
     useEffect(() => {
-        slider()?.addEventListener('scroll', listenToScroll)
+        slider?.current?.addEventListener('scroll', listenToScroll)
 
         return () => {
-            slider()?.removeEventListener('scroll', listenToScroll)
+            slider?.current?.removeEventListener('scroll', listenToScroll)
         }
     })
 
     return <div className={cn('relative', className)}>
-        <div id="cards-slider" className="whitespace-nowrap overflow-x-scroll overflow-y-hidden no-scrollbar">
+        <div ref={slider} className="whitespace-nowrap overflow-x-scroll overflow-y-hidden no-scrollbar">
             {games.map((game, index) => (
                 <GameCard key={game.id} game={game} className="inline-block mr-2 md:mr-3 last:mr-0" lazy={lazy} lists={lists} />
             ))}
@@ -67,24 +86,26 @@ const CardsSlider = ({ games, className, lazy, lists }: Props) => {
         <div className="controls absolute w-full flex justify-between h-full -top-1 pointer-events-none">
             <SliderControl direction="left" onClick={scrollLeft} className={cn(
                 scrollPosition === 'left' && 'opacity-0 pointer-events-none'
-            )}/>
+            )} onMouseEnter={() => autoScroll('left')} onMouseLeave={stopAutoScroll}/>
             <SliderControl direction="right" onClick={scrollRight} className={cn(
                 scrollPosition === 'right' && 'opacity-0 pointer-events-none'
-            )}/>
+            )} onMouseEnter={() => autoScroll('right')} onMouseLeave={stopAutoScroll}/>
         </div>
     </div>
 }
 
-const SliderControl = ({ direction, onClick, className }: {
+const SliderControl = ({ direction, onClick, className, onMouseEnter, onMouseLeave }: {
     direction: 'left' | 'right',
     onClick: () => void,
+    onMouseEnter: () => void,
+    onMouseLeave: () => void,
     className?: string
 }) => {
     return <span className={cn(
         'h-full z-10 w-7 md:w-10 bg-background opacity-80 transition-opacity flex justify-center items-center pointer-events-auto cursor-pointer',
         className
-    )} onClick={onClick}>
-        <Chevron direction={direction} className="w-6 md:w-8 h-6 md:h-8 ml-1 opacity-100"/>
+    )} onClick={onClick} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+        <Chevron direction={direction} className={cn('w-6 md:w-8 h-6 md:h-8 opacity-100', direction === 'left' ? 'mr-1' : 'ml-1')}/>
     </span>
 }
 
